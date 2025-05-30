cef47645c399889a2e361716a72e58108b587279
Smartguy903 <adsweeney@augusta.edu>
2025-05-30 11:43:55 -0400

    Update einstein.py

--- 03-einstein/einstein.py
+++ 03-einstein/einstein.py
@@ -1,9 +1,10 @@
 # Prompt the user for mass as an integer (in kilograms)
-
+m = int(input("Enter mass in kilograms: "))
 
 # Calculate the Einstein formula (don't forget to convert the input from str to
 # int)
-
+c = 300000000
+answer = (m * c * c)
 
 # Print the result
-
+print (answer)
