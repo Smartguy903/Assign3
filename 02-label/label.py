1109cdd55993a853ffdb8b6b56acf6408c7d61b1
Smartguy903 <adsweeney@augusta.edu>
2025-05-30 09:17:27 -0400

    Update label.py
    It should all be correct now

--- 02-label/label.py
+++ 02-label/label.py
@@ -1,14 +1,14 @@
 # Prompt the user to for a first name
-
+first_name = input("Enter a first name: ")
 
 # Prompt the user to for a last name
-
+last_name = input("Enter a last name: ")
 
 # Prompt the user for a job title
-
-
+job_title = input("Enter a job title: ")
 # Print the values label in the form:
 #
 # last_name, first_name
+print (last_name + "," + first_name)
 # title
-
+print (job_title)
